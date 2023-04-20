9a37b0c16104f2421774e92d9ba197ec40b6c069
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-04-20 23:14:16 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-import Loader from './Loader/Loader';
-import Button from './Button/Button';
-import Modal from './Modal/Modal';
+// import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+// import Loader from './Loader/Loader';
+// import Button from './Button/Button';
+// import Modal from './Modal/Modal';
 
 export class App extends Component {
 
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 export class ImageGallery extends Component  {
 
   state = {
